e49904cca4844a504bdf0e2c499719dab3cbdca4
Alexander Celis <dacelis0@misena.edu.co>
2020-05-18 17:36:28 -0500

    changed functions and variables
    changed:
    -functiones to variables
    -minify js and css
    -create arrow funcions

--- js/calculator.js
+++ js/calculator.js
@@ -1,126 +1,109 @@
-//Llamar los elementos principales
-let displayBox = document.querySelector('.displayBox'),
-  keyboard = document.querySelector('.keyboard'),
-  status = false,
-  n1 = 0,
-  typeOperation = ''
-  
-//Inicializar la calculadora en cero
-displayBox.textContent = 0
+(function () {
+  const getClass = document.querySelector.bind(document)
 
-//Que botón del teclado estoy presionando
-function calculator() {
-  //Si la operación devuelve null, no ejecute nada
-  if (!keyboard) {
-    return
-  }
-  keyboard.addEventListener('click', function (e) {
-    //target, dataset
-    let t = e.target,
+  let displayBox = getClass('.displayBox'),
+    keyboard = getClass('.keyboard'),
+    status = false,
+    n1 = 0,
+    typeOperation = ''
+
+  displayBox.textContent = 0
+
+  let calculator = () => {
+    if (!keyboard) {
+      return
+    }
+    keyboard.addEventListener('click', e => {
+      let t = e.target,
         ds = t.dataset
-    //que número estoy pulsando
-    if (ds.number) {
 
-      //Escribe en pantalla
-      writeBox(ds.number)
-    }
-    //que operación estoy pulsando
-    if (ds.math) {
-
-      //Llama la operación
-      getMath(t, ds.math)
-    }
-    //igualdad-limpiar
-    if (ds.operation) {
-      
-      //Ejecuta la operación
-      runMath(ds.operation)
-    }
-    if(ds.symbol){
-      displayBox.textContent = -(displayBox.textContent)
-    }
-  })
-}
-
-//Imprimir los números en pantalla
-function writeBox(number) {
-  //Preguntamos si el valor iniciar el cero
-  if (displayBox.textContent === '0' || status === true) {
-    displayBox.textContent = number
-  } else {
-    if(number === '.' && !displayBox.textContent.includes('.')){
-      displayBox.textContent += number
-    }else if(number !== '.'){
-      displayBox.textContent += number
-    }else{
-      null
-    }
+      if (ds.number) {
+        writeBox(ds.number)
+      }
+      if (ds.math) {
+        getMath(t, ds.math)
+      }
+      if (ds.operation) {
+        runMath(ds.operation)
+      }
+      if (ds.symbol) {
+        displayBox.textContent = -(displayBox.textContent)
+      }
+    })
   }
-  status = false
-}
 
-//Llamamos la operación a ejecutar
-function getMath(ele, operation) {
-  status = true
-  //Capturamos el primer numero
-  n1 = Number(displayBox.textContent)
-  typeOperation = operation
-  //Modificamos la pantalla con la operación
-  displayBox.textContent = ele.textContent
-}
-
-//Ejecuta las operación de igual y c
-function runMath(operation) {
-  function getResult(n1, typeOperation) {
-    let n2 = Number(displayBox.textContent),
-        result = 0
-    //que tipo de operación estoy ejecutando
-    switch (typeOperation) {
-      case 'add':
-        result = n1 + n2
-        break
-      case 'minus':
-        result = n1 - n2
-        break
-      case 'multiply':
-        result = n1 * n2
-        break
-      case 'divide':
-        result = n1 / n2
-        break
-      case 'percnt':
-        result = (n1 / 100) * n2
-        break
-      default:
-        break
+  let writeBox = (number) => {
+    if (displayBox.textContent === '0' || status === true) {
+      displayBox.textContent = number
+    } else {
+      if (number === '.' && !displayBox.textContent.includes('.')) {
+        displayBox.textContent += number
+      } else if (number !== '.') {
+        displayBox.textContent += number
+      } else {
+        null
+      }
     }
-    if (result === Infinity) {
-      displayBox.textContent = 'Error'
-    }else{
-      let textResult = result.toString() //toString() -> Convierte dato en cadena de texto
+    status = false
+  }
 
-      if(textResult.includes('.') === true){
+  let getMath = (ele, operation) => {
+    status = true
+    n1 = Number(displayBox.textContent)
+    typeOperation = operation
+    displayBox.textContent = ele.textContent
+  }
 
-        if (textResult.length <= 10 && textResult.length >= 0) {
-          displayBox.textContent = textResult.slice(0,5)
-        }else{
-          displayBox.textContent = textResult.slice(0,10)
-        }
-      }else{
+  let runMath = operation => {
+    let getResult = (n1, typeOperation) => {
+      let n2 = Number(displayBox.textContent),
+        result = 0
 
-        displayBox.textContent = textResult
+      switch (typeOperation) {
+        case 'add':
+          result = n1 + n2
+          break
+        case 'minus':
+          result = n1 - n2
+          break
+        case 'multiply':
+          result = n1 * n2
+          break
+        case 'divide':
+          result = n1 / n2
+          break
+        case 'percnt':
+          result = (n1 / 100) * n2
+          break
+        default:
+          break
       }
 
+      if (result === Infinity) {
+        displayBox.textContent = 'Error'
+      } else {
+        let textResult = result.toString()
+        if (textResult.includes('.') === true) {
+          if (textResult.length <= 10 && textResult.length >= 0) {
+            displayBox.textContent = textResult.slice(0, 5)
+          } else {
+            displayBox.textContent = textResult.slice(0, 10)
+          }
+        } else {
+          displayBox.textContent = textResult
+        }
+      }
     }
-  }
-  //Limpiar la pantalla
-  if (operation === 'empty') {
-    displayBox.textContent = '0'
-    result = 0
-  } else {
-    getResult(n1, typeOperation)
-  }
-  status = true
-}
 
-calculator()
+    if (operation === 'empty') {
+      displayBox.textContent = '0'
+      result = 0
+    } else {
+      getResult(n1, typeOperation)
+    }
+
+    status = true
+  }
+
+  calculator()
+}(c = e => { console.log(e) }))
